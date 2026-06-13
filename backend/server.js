aa8c416ba168545c6a6efe89ccd7b6ff8eb3ab1d
manikantavaraprasad2130 <manikantavaraprasad2130@users.noreply.github.com>
2026-06-13 13:22:18 +0530

    done deployment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.use(cors());
+app.use(cors({origin :"https://nestora-connect-uvbq.vercel.app"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(uploadDirectory));
